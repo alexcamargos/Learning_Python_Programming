e0ed564293c7a4486f9b1ce0cf7465b348845ebd
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2021-04-09 12:10:01 -0300

    Added method to hash multiple files.

--- FileHash/file_hash.py
+++ FileHash/file_hash.py
@@ -18,6 +18,7 @@
 """Python project to facilitate the generation of hash/checksum for files and directorys."""
 
 import hashlib
+from collections import namedtuple
 
 _hash_algorithm_map = {
     # 'adler32': zlib.adler32,
@@ -32,8 +33,11 @@ _hash_algorithm_map = {
 
 SUPPORTED_ALGORITHMS = set(_hash_algorithm_map.keys())
 
+FileHashResult = namedtuple('FileHashResult', ['file_name', 'hash'])
+
 
 class FileHash:
+    """Wrapping the hashlib module to facilitate calculating file hashes."""
 
     def __init__(self, hash_algorithm='sha512', chunk_size=65536):
         """"Initialize the class."""
@@ -41,7 +45,10 @@ class FileHash:
         if hash_algorithm not in SUPPORTED_ALGORITHMS:
             raise ValueError(f'Error, unsupported hash/checksum algorithm: {hash_algorithm}')
 
+        # The hash algorithm to use.
         self.hash_algorithm = hash_algorithm
+
+        # The chunk size (in bytes) when reading files.
         self.chunk_size = chunk_size
 
     def generate_file_hash(self, file_name):
@@ -65,6 +72,15 @@ class FileHash:
 
         return file_hash.hexdigest()
 
+    def generate_files_hash(self, file_names):
+        """Generate the hash of of multiple files."""
+
+        return [FileHashResult(fname, self.generate_file_hash(fname)) for fname in file_names]
+
 
 hash_file = FileHash()
 print(hash_file.generate_file_hash(r'D:\_Teste_FileFinder\rufus-3.9p.exe'))
+
+files = [r'D:\_Teste_FileFinder\README.md', r'D:\_Teste_FileFinder\cpython.png']
+for i in hash_file.generate_files_hash(files):
+    print(i.file_name, i.hash)

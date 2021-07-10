6f3fc6386bc33a1ebd41f0e98efca8794537fcd2
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2021-07-10 16:12:40 -0300

    Project HTMLBaseTemplate refactor.

--- HTMLBaseTemplate/html_base_template.py
+++ HTMLBaseTemplate/html_base_template.py
@@ -14,14 +14,25 @@
 
 import os
 
-root_files = ['index.html', 'README.md', 'LICENSE']
-css_files = ['main.css']
+root_files = ['index.html',
+              'README.md',
+              'LICENSE',
+              '.gitignore',
+              '.editorconfig']
 
-root_dirs = ['assets']
-sub_dirs = ['javascript', 'img', 'stylesheet']
+template_files = ['html',
+                  'readme',
+                  'license']
+
+stylesheet_files = ['main.css']
+
 javascript_files = None
 img_files = None
-stylesheet_files = ['main.css']
+
+root_dirs = ['assets']
+sub_dirs = ['javascript',
+            'stylesheet',
+            'img']
 
 copyright_banner = """/*
 # -------------------------------------------------------------------------------------------------
@@ -29,8 +40,8 @@ copyright_banner = """/*
 # Name: main.css
 # Version: 0.0.1
 #
-# Summary: 
-#         
+# Summary:
+#
 #
 # Author: Alexsander Lopes Camargos
 # Author-email: alcamargos@vivaldi.net
@@ -40,107 +51,50 @@ copyright_banner = """/*
 # -------------------------------------------------------------------------------------------------
 */"""
 
-html_template = """<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+def create_file(file_name, content):
+    """Create the file in the directory using the given content."""
 
-<body>
+    with open(file_name, 'w') as _fn:
+        _fn.write(content)
 
-</body>
 
-</html>"""
+with open(r'D:\WORKSPACE\Python\LearningPythonProgramming\HTMLBaseTemplate\template\html') as _fn:
+    html_template = _fn.read()
 
-readme_template = """# Project Name
-Project description.
+with open(r'D:\WORKSPACE\Python\LearningPythonProgramming\HTMLBaseTemplate\template\readme') as _fn:
+    readme_template = _fn.read()
 
-## View Project in Execution
-You can check this website on this [link](https://#).
+with open(r'D:\WORKSPACE\Python\LearningPythonProgramming\HTMLBaseTemplate\template\license') as _fn:
+    license_template = _fn.read()
 
-## Technologies
-- HTML5
-- CSS3
-- JavaScript
+with open(r'D:\WORKSPACE\Python\LearningPythonProgramming\HTMLBaseTemplate\template\gitignore') as _fn:
+    gitignore_template = _fn.read()
 
-## Prerequisites
-You need a modern browser, just clone the repository, open the index.html
-and edit it as you like. After edit share with me your version. :happy:
-
-## License
-
-Copyright (c) 2021 - **Alexsander Lopes Camargos**
-
-Permission is hereby granted, free of charge, to any person obtaining a 
-copy of this software and associated documentation files (the "Software"), 
-to deal in the Software without restriction, including without limitation 
-the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-and/or sell copies of the Software, and to permit persons to whom the 
-Software is furnished to do so, subject to the following conditions: 
-
-The above copyright notice and this permission notice shall be included in 
-all copies or substantial portions of the Software. 
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-DEALINGS IN THE SOFTWARE. """
-
-license_template = """The MIT License (MIT)
-
-Copyright (c) 2021 - Alexsander Lopes Camargos
-
-Permission is hereby granted, free of charge, to any person obtaining a 
-copy of this software and associated documentation files (the "Software"), 
-to deal in the Software without restriction, including without limitation 
-the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-and/or sell copies of the Software, and to permit persons to whom the 
-Software is furnished to do so, subject to the following conditions: 
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-DEALINGS IN THE SOFTWARE. """
-
+with open(r'D:\WORKSPACE\Python\LearningPythonProgramming\HTMLBaseTemplate\template\editorconfig') as _fn:
+    editorconfig_template = _fn.read()
 
 for file in root_files:
     if file == 'index.html':
-        with open(file, 'w') as html_file:
-            html_file.write(html_template)
+        create_file(file, html_template)
     elif file == 'README.md':
-        with open(file, 'w') as readme_file:
-            readme_file.write(readme_template)
+        create_file(file, readme_template)
+    elif file == '.gitignore':
+        create_file(file, gitignore_template)
+    elif file == '.editorconfig':
+        create_file(file, editorconfig_template)
     else:
-        with open(file, 'w') as license_file:
-            license_file.write(license_template)
-
+        create_file(file, license_template)
 
 for base_dir in root_dirs:
     os.mkdir(base_dir)
 
-
 os.chdir('assets')
-
 
 for sub_dir in sub_dirs:
     os.mkdir(sub_dir)
 
-
 os.chdir('stylesheet')
-
 
 for file in stylesheet_files:
     with open(file, 'w') as stylesheet:

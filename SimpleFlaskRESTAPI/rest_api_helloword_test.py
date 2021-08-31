3e917b5ab2e9ce8513cc42135a38f50d45a17de4
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2021-08-31 11:25:12 -0300

    Simple REST API with Flask
    feat: Testing a Hello Word simple REST API.

---
+++ SimpleFlaskRESTAPI/rest_api_helloword_test.py
@@ -0,0 +1,33 @@
+#  #!/usr/bin/env python
+#  encoding: utf-8
+#
+#  ---------------------------------------------------------------------------
+#  Name: main.py
+#  Version: 0.0.1
+#  Summary: Testing a simple REST API making with Flask.
+#
+#  Author: Alexsander Lopes Camargos
+#  Author-email: alcamargos@vivaldi.net
+#
+#  License: MIT
+#  ---------------------------------------------------------------------------
+
+"""Testing a Hello Word simple REST API making with Flask."""
+
+import requests
+
+BASE_URL = 'http://127.0.0.1:5000/'
+ENDPOINT = 'helloword'
+REQUEST_URL = BASE_URL + ENDPOINT
+
+with requests.get(REQUEST_URL) as response_get:
+    print(response_get.json())
+
+with requests.post(REQUEST_URL) as response_post:
+    print(response_post.json())
+
+with requests.put(REQUEST_URL) as response_put:
+    print(response_put.json())
+
+with requests.delete(REQUEST_URL) as response_delete:
+    print(response_delete.json())
